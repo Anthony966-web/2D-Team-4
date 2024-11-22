683a37ab8455f25f8c14ae860d38b99ab4141797
MUKILTEO\1720941 <jtkid2@icloud.com>
2024-11-22 10:23:22 -0800

    adding Pew pews

--- 2D Template/Assets/Prefabs/Weapons/Shooting.cs
+++ 2D Template/Assets/Prefabs/Weapons/Shooting.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Transform FirePiont;
+    public GameObject BulletPrefab;
+
+    public float bulletForce = 20f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, FirePiont.position, FirePiont.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(FirePiont.up * bulletForce, ForceMode2D.Impulse);
     }
 }
