66f2937575b989fd8f10e0cf798e531d83c9eb0a
MUKILTEO\1722415 <1722415@mukilteo.wednet.edu>
2024-11-22 08:23:18 -0800

    Updated MovementScipt

--- 2D Template/Assets/Scripts/PlayerMovement.cs
+++ 2D Template/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
 
-    public Rigidbody2D rb;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Camera mainCamera;
 
-    Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+    }
 
-    // Update is called once per frame
     void Update()
     {
-       movement.x = Input.GetAxisRaw("Horizontal");
-       movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        RotateTowardsMouse();
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = movement * moveSpeed;
     }
 
+    void RotateTowardsMouse()
+    {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
 
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+        rb.rotation = angle;
+    }
 }
